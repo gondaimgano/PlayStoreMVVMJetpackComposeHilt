fc24a829a7a6587b5f83d42067dfc6370c0abe1b
gondaimgano <gondaimgano@gmail.com>
2021-10-11 09:51:53 +0200

    refactored

--- app/src/main/java/gondai/tutorial/playstoremvvmjetpackcomposehilt/screens/product/ProductView.kt
+++ app/src/main/java/gondai/tutorial/playstoremvvmjetpackcomposehilt/screens/product/ProductView.kt
@@ -1,10 +1,15 @@
 package gondai.tutorial.playstoremvvmjetpackcomposehilt.screens.product
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -15,17 +20,21 @@ fun ProductView(productModel:ProductViewModel= hiltViewModel(),item:String) {
     val products = productModel.getProductPagination(item).collectAsLazyPagingItems()
 
     Column(
-        modifier = Modifier.padding(8.dp).fillMaxHeight(0.18f)
+      //  modifier = Modifier.padding(8.dp).fillMaxHeight(0.18f)
     ) {
         Text(item)
         Spacer(modifier = Modifier.padding(8.dp))
         LazyRow{
             items(products){
                     product->
-                Text(product?:"Empty",
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(0.18f))
+               Card(modifier = Modifier.size(100.dp).clip(
+                   RoundedCornerShape(20.dp)).padding(10.dp),
+               backgroundColor = Color.Cyan){
+                   Text(product?:"Empty",
+                       modifier = Modifier
+                           .padding(8.dp)
+                           .fillMaxWidth(0.18f))
+               }
             }
         }
 
